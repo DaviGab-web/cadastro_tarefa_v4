3447b873108af88fb82f95302755fcf8b42cf5ec
DaviGab-web <d3bigithub@gmail.com>
2025-07-28 11:12:12 -0300

    erro corrigido

--- backend/routes/tarefaRoutes.js
+++ backend/routes/tarefaRoutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/tarefaController");
 const autenticarToken = require("../middlewares/authMiddleware");
 const tarefaController = require("../controllers/tarefaController");
 
-
-router.get("/",autenticarToken, tarefaController);
-router.post("/",autenticarToken, tarefaController);
-router.patch("/:id",autenticarToken, tarefaController);
-router.delete("/:id",autenticarToken, tarefaController);
+// Rotas com as funções corretas do controller
+router.get("/", autenticarToken, tarefaController.listar);
+router.post("/", autenticarToken, tarefaController.criar);
+router.patch("/:id", autenticarToken, tarefaController.alterarStatus);
+router.delete("/:id", autenticarToken, tarefaController.remover);
 
 module.exports = router;
